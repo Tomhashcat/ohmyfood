0a3d307d4d6b380590c920a5b8a27cdaf50ec2fc
ThomasHascat <dutourne.t@gmail.com>
2023-05-31 18:46:10 +0200

    :fix retrait du js pour le loader impossible de le faire disparaitre

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
-const loader = document.querySelector('.loader');
+/*const loader = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
 
     loader.classList.add('fondu-out');
 
-})
+})*/
